bc7d7cdbe70938e2f9154107c40039ea19ca43aa
GUILHERME VIEIRA <gv524003@gmail.com>
2025-03-22 00:46:16 -0300

    Create exerc1.java

---
+++ vetor/exerc1.java
@@ -0,0 +1,31 @@
+package aplicação;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class programnegativos {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		int n;
+		System.out.println("Quantos numeros voce vai digitar?: ");
+		 n=sc.nextInt();
+		 
+		 int[] vetor = new int[n];
+		 for(int i=0; i<n; i++) {
+			 System.out.println("Digite um numero: ");
+			 vetor[i]=sc.nextInt();
+		 }
+		 System.out.println("Numeros negativos: ");
+		 for (int i=0; i<n; i++) {
+			 if(vetor[i]<0) {
+				 System.out.printf("%d\n", vetor[i]);
+			 }
+		 }
+		 sc.close();
+		 
+		
+	}
+}
