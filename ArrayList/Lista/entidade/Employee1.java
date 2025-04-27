7e5a0f6c3354b929398256092edcebf8e6d09d7c
GUILHERME VIEIRA <gv524003@gmail.com>
2025-04-26 23:42:09 -0300

    Update Employee1.java

--- ArrayList/Lista/entidade/Employee1.java
+++ ArrayList/Lista/entidade/Employee1.java
@@ -1,34 +1,49 @@
+// Pacote de entidades
 package entidades;
 
+// Classe que representa um funcionário
 public class Employee1 {
-	private Integer Id;
-	private String name;
-	private double salary;
-	public Employee1(Integer id, String name, double salary) {
-		super();
-		Id = id;
-		this.name = name;
-		this.salary = salary;
-	}
-	public Integer getId() {
-		return Id;
-	}
-	public void setId(Integer id) {
-		Id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public double getSalary() {
-		return salary;
-	}
-	public void increaseSalary(double percentage){
-		salary += salary * percentage / 100.0; 
-	}
-	public String toString() {
-		return Id + "," +name+ "," +String.format("%.2f", salary);	
-		}
+    private Integer Id; // Atributo para id do funcionário
+    private String name; // Atributo para nome do funcionário
+    private double salary; // Atributo para salário do funcionário
+
+    // Construtor
+    public Employee1(Integer id, String name, double salary) {
+        super();
+        Id = id;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    // Métodos getters e setters
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    // Método para aumentar o salário com base em uma porcentagem
+    public void increaseSalary(double percentage) {
+        salary += salary * percentage / 100.0;
+    }
+
+    // Método toString para exibir o funcionário no formato: id, nome, salário
+    @Override
+    public String toString() {
+        return Id + "," + name + "," + String.format("%.2f", salary);
+    }
 }
