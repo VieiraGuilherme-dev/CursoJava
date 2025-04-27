3b3cabb92d59a5836069777544c4cc033311d556
GUILHERME VIEIRA <gv524003@gmail.com>
2025-04-26 23:46:39 -0300

    Update Product.java

--- Herança e polimorfismo/entities/Product.java
+++ Herança e polimorfismo/entities/Product.java
@@ -1,34 +1,45 @@
+// Pacote onde a classe está localizada
 package entities;
 
+// A classe Product representa um produto com nome e preço
 public class Product {
 
+    // Nome do produto
     private String name;
+    
+    // Preço do produto
     private Double price;
 
+    // Construtor que inicializa nome e preço do produto
     public Product(String name, Double price) {
-        this.name = name;
-        this.price = price;
+        this.name = name; // Inicializa o nome do produto
+        this.price = price; // Inicializa o preço do produto
     }
 
+    // Getter para o nome do produto
     public String getName() {
         return name;
     }
 
+    // Setter para o nome do produto
     public void setName(String name) {
         this.name = name;
     }
 
+    // Getter para o preço do produto
     public Double getPrice() {
         return price;
     }
 
+    // Setter para o preço do produto
     public void setPrice(Double price) {
         this.price = price;
     }
 
+    // Método para exibir a etiqueta de preço do produto, formatado
     public String priceTag() {
-        return name
-                + " $ "
-                + String.format("%.2f", price);
+        return name // Nome do produto
+                + " $ " // Sinal de dólar
+                + String.format("%.2f", price); // Preço formatado com 2 casas decimais
     }
 }
