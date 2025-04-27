87172c3252886543ddd0732ab7a2ba5acfe5dd49
GUILHERME VIEIRA <gv524003@gmail.com>
2025-04-26 23:45:01 -0300

    Update Company.java

--- Herança e polimorfismo/entities/Company.java
+++ Herança e polimorfismo/entities/Company.java
@@ -1,27 +1,37 @@
+// Pacote onde a classe está localizada
 package entities;
 
-public class Company extends TaxPayer{
+// A classe Company herda de TaxPayer (pagador de impostos)
+public class Company extends TaxPayer {
+    
+    // Número de empregados da empresa
     private Integer numberOfEmployees;
 
+    // Construtor da classe Company, recebendo nome, renda anual e número de empregados
     public Company(String name, Double anualIncome, Integer numberOfEmployees) {
-        super(name, anualIncome);
-        this.numberOfEmployees = numberOfEmployees;
+        super(name, anualIncome); // Chama o construtor da superclasse TaxPayer
+        this.numberOfEmployees = numberOfEmployees; // Inicializa o número de empregados
     }
 
+    // Getter para o número de empregados
     public Integer getNumberOfEmployees() {
         return numberOfEmployees;
     }
 
+    // Setter para o número de empregados
     public void setNumberOfEmployees(Integer numberOfEmployees) {
         this.numberOfEmployees = numberOfEmployees;
     }
 
+    // Implementação do método abstrato tax() da superclasse
     @Override
-    public double tax(){
-        if(numberOfEmployees > 10){
-            return getAnualIncome()* 0.14;
+    public double tax() {
+        // Se a empresa tiver mais de 10 empregados, paga 14% de imposto
+        if (numberOfEmployees > 10) {
+            return getAnualIncome() * 0.14;
         }
-        else{
+        // Caso contrário, paga 16% de imposto
+        else {
             return getAnualIncome() * 0.16;
         }
     }
