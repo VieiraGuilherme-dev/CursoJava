2d8c3d0792abd87bd48688a0fa280bb426b73827
GUILHERME VIEIRA <gv524003@gmail.com>
2025-04-26 23:52:42 -0300

    Update Rectangle.java

--- POO/entidades/Rectangle.java
+++ POO/entidades/Rectangle.java
@@ -1,17 +1,46 @@
 package entidades;
 
 public class Rectangle {
-    public double width;
-    public double height;
-    public double area() {
-    return width * height;
-}
-  
-   public double perimeter() {
-   return 2 * (width + height);
-}
-   public double diagonal() {
-   return Math.sqrt(width * width + height * height);
+    
+    // Encapsulando as variáveis para garantir que só sejam modificadas de forma controlada
+    private double width;
+    private double height;
 
-   }
+    // Construtores para inicializar o objeto com os valores de largura e altura
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    // Getters e Setters para acessar e modificar a largura e altura de forma controlada
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    // Método para calcular a área do retângulo
+    public double area() {
+        return width * height;
+    }
+
+    // Método para calcular o perímetro do retângulo
+    public double perimeter() {
+        return 2 * (width + height);
+    }
+
+    // Método para calcular a diagonal do retângulo
+    public double diagonal() {
+        return Math.sqrt(width * width + height * height);
+    }
 }
